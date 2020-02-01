f333c8e4886ce595fd5fb399b3834c611b5c6ba8
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 16:04:41 -0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,3 +1,7 @@
+import requests
 
-url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+city = input('Enter your city name: ')
+
+
+url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
 
