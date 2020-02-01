d14ea36d86e3d92507c8cb7c874f23f0e1668b78
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 16:31:01 -0500

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -5,5 +5,5 @@ def test():
   cityNames = ["London", "Beijing", "New York", "Tokyo", "Hangzhou", "Boston", "Seattle"]
   
   for a in cityNames:
-	#assert weatherAPI.cityName("London")
-	weatherAPI.searchname(a)
+    #assert weatherAPI.cityName("London")
+    weatherAPI.searchname(a)
