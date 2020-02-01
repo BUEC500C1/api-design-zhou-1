06afbf13f8200d21b580617253fd9cdc49b1ce52
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 18:34:21 -0500

    Delete test_0.py

--- tests/test_0.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import weatherAPI
-
-def test():
-  cityNames = ["London", "Beijing", "New York", "Tokyo", "Hangzhou", "Boston", "Seattle"]
-  
-  for a in cityNames:
-    #assert weatherAPI.cityName("London")
-    weatherAPI.searchname(a)
