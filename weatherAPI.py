05a0cd95cb7aeb86bfcef1813cafde886a67aa6d
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 17:31:55 -0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -4,12 +4,10 @@ from pprint import pprint
 def searchname(cName):
   city = cName
   
-  url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22&units=metric'.format(city)
+  url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
 
   res = requests.get(url) 
   data = res.json()
-  
-  print(data)
   
   temp = data['main']['temp']
   temp_min = data['main']['temp_min']
