4c8f40ca00af162b3bb1372757e394da27983746
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 16:07:51 -0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,9 +1,10 @@
 import requests
 
-city = input('Enter your city name: ')
+def cityName(cName):
+  city = input('Enter your city name: ')
+  
+  url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
 
-url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
+  res = requests.get(url)
 
-res = requests.get(url)
-
-print(res)
+  print(res)
