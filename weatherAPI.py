3182e32e8112a7799f4833c03c5acb3cf712a28b
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 18:15:07 -0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 
+# test use
 def searchname(cName):
   city = cName
   
@@ -29,3 +30,44 @@ def searchname(cName):
   file1.write("Description of weather there is {} \n".format(description) )
   
   file1.close()
+
+  
+# def for get lati and longi from airport's name
+# def getdeatiledlocation(aName):
+
+
+# search based on lati and longi
+def searchdimension(lati, longi):
+  latitude = lati
+  longitude = longi
+  
+  file1 = open("results/results.txt","w") 
+  
+  # Not use &units=metric, 404 error
+  url = 'https://samples.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=b6907d289e10d714a6e88b30761fae22'.format(latitude, longitude)
+
+  res = requests.get(url) 
+  data = res.json()
+  
+  temp = data['main']['temp']
+  temp_min = data['main']['temp_min']
+  temp_max = data['main']['temp_max']
+  pressure = data['main']['pressure']
+  wind_speed = data['wind']['speed']
+  description = data['weather'][0]['description']
+
+  file1.write("temp there is {} \n".format(temp) )
+  file1.write("Min temp there is {} \n".format(temp_min) )
+  file1.write("Max temp there is {} \n".format(temp_max) )
+  file1.write("Pressure there is {} \n".format(pressure) )
+  file1.write("Speed of wind there is {} \n".format(wind_speed) )
+  file1.write("Description of weather there is {} \n".format(description) )
+  
+  file1.close()
+
+  
+  
+  
+  
+  
+  
