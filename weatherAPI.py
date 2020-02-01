9773de26482474f2b536b5fb73938c403509abdb
Zhou <gentle_zhou_shen@outlook.com>
2020-02-01 17:36:31 -0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -4,6 +4,7 @@ from pprint import pprint
 def searchname(cName):
   city = cName
   
+  # Not use &units=metric, 404 error
   url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'.format(city)
 
   res = requests.get(url) 
@@ -14,7 +15,7 @@ def searchname(cName):
   temp_max = data['main']['temp_max']
   pressure = data['main']['pressure']
   wind_speed = data['wind']['speed']
-  description = data['weather']['0']['description']
+  description = data['weather'][0]['description']
 
   # pprint(data)
   print("Weather in {} is: ".format(city) )
